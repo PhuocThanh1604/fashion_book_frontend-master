6f4b734ab906c2866d65b573cad5aed2171c56fb
phamhuuphuc <phucphse150590@fpt.edu.vn>
2023-03-14 16:09:44 +0700

    fix footer middle

--- src/components/footer/footer.middle.js
+++ src/components/footer/footer.middle.js
@@ -8,10 +8,10 @@
 							<div className="single-widget">
 								<h2>Thông Tin Về Chúng Tôi</h2>
 								<ul className="nav nav-pills nav-stacked ">
-									<li><i className='fas fa-map-marker-alt'></i><a href="#">Hà Nội City</a></li>
-									<li><i className='far fa-envelope'></i><a href="#">dimdimliulo@gmail.com</a></li>
+									<li><i className='fas fa-map-marker-alt'></i><a href="#">HCM City</a></li>
+									<li><i className='far fa-envelope'></i><a href="#">aresgaming607@gmail.coim</a></li>
 									<li><i className='fas fa-phone-volume'></i><a href="#">+84 234 567 89</a></li>
-									<li><i class="fab fa-facebook-f"></i><a href="#">dimdimliulo</a></li>
+									<li><i class="fab fa-facebook-f"></i><a href="#">Phạm Hữu Phúc</a></li>
 									
 								</ul>
 							</div>
